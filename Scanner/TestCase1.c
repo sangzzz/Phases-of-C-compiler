3b90422f6852d663ddb0a34acde009d5f9476b89
irfanbacker <irfuvk@gmail.com>
2020-08-20 23:23:04 +0530

    Basic scanner

---
+++ Scanner/TestCase1.c
@@ -0,0 +1,20 @@
+// asas
+/*
+
+
+            */
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 's'; i <= n; ++i)
+    {
+        sum += i;
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
+/* *****hjkdkj******bnm */
